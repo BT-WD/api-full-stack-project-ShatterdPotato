f175aac3b475a077891527a2afe822240ecb6080
Sameer Ahmed <sameera22@nycstudents.net>
2026-04-20 13:41:39 -0400

    bacend deveped to allowed swithcing between views

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const dashboardView = document.getElementById("dashboard-view");
 
+function onSignIn(view) {
+    const createView = document.getElementById(view);
+    createView.style.display = "none"
+    dashboardView.style.display = "block";
+    const email = document.getElementById("email-field").value
+    const usernameElement = document.getElementById("username");
+    usernameElement.textContent = email; 
+}
+
+function onLogout() {
+    document.getElementById("login-view").style.display = "";
+    dashboardView.style.display = "none";
+}
+
+function onSignUp() {
+    document.getElementById("login-view").style.display = "none"
+    document.getElementById("auth-view").style.display = ""
+}
+
+function onCreate() {
+    document.getElementById("login-view").style.display = ""
+    document.getElementById("auth-view").style.display = "none"
+}
