8e5d96003d3465fb84ba8a2a297d56370ab54a29
Sameer Ahmed <sameera22@nycstudents.net>
2026-04-22 13:13:23 -0400

    api backend complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const dashboardView = document.getElementById("dashboard-view");
+const api_key = "fa8348adfd7223a66deebf0baedd684f"
 
 function onSignIn(view) {
     const createView = document.getElementById(view);
@@ -37,6 +38,24 @@ fileInput.addEventListener('change', function() {
     if (this.files && this.files[0]) {
         const selectedFile = this.files[0];
         alert("You selected: " + selectedFile.name);
-        
+        getIMGLink(selectedFile)
     }
 });
+
+async function getIMGLink(selectedFile) {
+    const url = "https://api.imgbb.com/1/upload"
+    const formData = new FormData();
+    formData.append('image', selectedFile);
+    const params = `?key=${api_key}`
+    const response = await fetch(url + params, {
+        method: 'POST',
+        body: formData
+    })
+
+   
+    if (response.ok) {
+        const responseJSON = await response.json()
+        const imgURL = responseJSON.data.url
+        console.log(imgURL)
+    }
+}
