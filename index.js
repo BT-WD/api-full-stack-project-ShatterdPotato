e0e68107b5f3bec3e20bb4b4817d2e888fea4e64
Sameer Ahmed <sameera22@nycstudents.net>
2026-04-22 12:50:32 -0400

    upload functionality added

--- index.js
+++ index.js
@@ -23,3 +23,20 @@ function onCreate() {
     document.getElementById("login-view").style.display = ""
     document.getElementById("auth-view").style.display = "none"
 }
+
+
+const fileInput = document.getElementById('file-input');
+const chooseFileBtn = document.getElementById('choose-file-btn');
+
+chooseFileBtn.addEventListener('click', () => {
+    fileInput.click();
+});
+
+
+fileInput.addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+        const selectedFile = this.files[0];
+        alert("You selected: " + selectedFile.name);
+        
+    }
+});
