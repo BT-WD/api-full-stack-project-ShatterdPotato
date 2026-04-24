858d00613234aaab05daa71e4dd2f7f1ae2e3470
Sameer Ahmed <sameera22@nycstudents.net>
2026-04-24 13:16:58 -0400

    Replaced the top leve event functions with arrow syntax functionsusing AddeventListner instead of inline HTML

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyBFxE7Zpz6tPiEJQtzidxRpG3kr2ocjW5g",
+authDomain: "hot-and-cold-81a78.firebaseapp.com",
+projectId: "hot-and-cold-81a78",
+storageBucket: "hot-and-cold-81a78.firebasestorage.app",
+messagingSenderId: "107139806240",
+appId: "1:107139806240:web:037f3851af00b443743845"
+};
+
+const app = initializeApp(firebaseConfig);
 const dashboardView = document.getElementById("dashboard-view");
 const api_key = "fa8348adfd7223a66deebf0baedd684f"
 
-function onSignIn(view) {
-    const createView = document.getElementById(view);
-    createView.style.display = "none"
+const fileInput = document.getElementById('file-input');
+const chooseFileBtn = document.getElementById('choose-file-btn');
+document.getElementById('logout-btn').addEventListener('click', () => {
+    document.getElementById("login-view").style.display = "";
+    dashboardView.style.display = "none";
+})
+
+document.getElementById('signup-btn').addEventListener('click', () => {
+    document.getElementById('auth-view').style.display = "none"
     dashboardView.style.display = "block";
     const email = document.getElementById("email-field").value
     const usernameElement = document.getElementById("username");
     usernameElement.textContent = email; 
-}
+})
 
-function onLogout() {
-    document.getElementById("login-view").style.display = "";
-    dashboardView.style.display = "none";
-}
-
-function onSignUp() {
+document.getElementById('signin-btn').addEventListener('click', () => {
     document.getElementById("login-view").style.display = "none"
-    document.getElementById("auth-view").style.display = ""
-}
+    dashboardView.style.display = "block";
+    const email = document.getElementById("email-field").value
+    const usernameElement = document.getElementById("username");
+    usernameElement.textContent = email; 
+})
 
-function onCreate() {
+document.getElementById('signin-link').addEventListener('click', () => {
     document.getElementById("login-view").style.display = ""
     document.getElementById("auth-view").style.display = "none"
-}
+})
 
-
-const fileInput = document.getElementById('file-input');
-const chooseFileBtn = document.getElementById('choose-file-btn');
+document.getElementById('signup-link').addEventListener('click', () => {
+    document.getElementById("login-view").style.display = "none"
+    document.getElementById("auth-view").style.display = ""
+})
 
 chooseFileBtn.addEventListener('click', () => {
     fileInput.click();
@@ -59,3 +76,4 @@ async function getIMGLink(selectedFile) {
         console.log(imgURL)
     }
 }
+
